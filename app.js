8edc0816824a2b6f42508c2cc20227d82b4f2474
Simeon Bateman <simeon@pnwrain.com>
2011-04-10 23:21:07 -0700

    exporting other types, added results for votes

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ server.listen(8080);
 // socket.io 
 var socket = io.listen(server); 
 
+	var results;
 	
   	// Add a connect listener
 	socket.on('connection', function(client){ 
@@ -21,11 +22,17 @@ var socket = io.listen(server);
 		
 	  // Success!  Now listen to messages to be received
 	  client.on('message',function(msg){ 
-		console.log( msg, msg.type, types.MESSAGE_TYPE_QUESTION);
-		//client.send({type:"message",foo:"aaron"});
+		console.log(msg.type);
 	    if (msg.type === types.MESSAGE_TYPE_QUESTION) {
+		 	results = {};
 			console.log('Sending Question');
 			client.broadcast(msg);
+		}
+		if (msg.type === types.MESSAGE_TYPE_VOTE) {
+			console.log('Sending Results');
+			console.log(msg);
+			results[msg.answer] += 1;
+			client.broadcast(results);
 		}
 	
 		//console.log('Received message from client!',msg);
